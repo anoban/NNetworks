882dd61a73831b7f7b35e3de75544cd45135187e
anoban <2016sb117@univ.jfn.ac.lk>
2024-02-27 14:31:26 +0530

    modified:   Fashion-MNIST/src/NNetwork.py 	modified:   Fashion-MNIST/src/main.py 	modified:   MNIST/src/NNetwork.py

--- Fashion-MNIST/src/main.py
+++ Fashion-MNIST/src/main.py
@@ -1,22 +1,8 @@
-from NNetwork import NNetworkMinimal
+from NNetwork import NNetworkExtended
 from IdxUtils import Idx1, Idx3
 
 def main() -> None:
-    
-    # MNIST training data
-    train_images = Idx3(r"../train-images-idx3-ubyte")
-    train_labels = Idx1(r"../train-labels-idx1-ubyte")
-    # MNIST test data
-    test_images = Idx3(r"../t10k-images-idx3-ubyte")
-    test_labels = Idx1(r"../t10k-labels-idx1-ubyte")
-    
-    model = NNetworkMinimal(nodes_in = 784, nodes_hid = 10, nodes_out = 10, alpha = 0.15, maxiterations = 1200)
-    model.gradient_descent(train_images.data, train_labels.data)
-    
-    print(f"Train-set accuracy: {model.accuracy_score(train_images.data, train_labels.data):.6f}")
-    print(f"Test-set accuracy: {model.accuracy_score(test_images.data, test_labels.data):.6f}")
-    # save the model in the parent directory, not inside /src
-    model.save("../fashion")
-    
+    pass
+
 if __name__ == "__main__":
     main()
