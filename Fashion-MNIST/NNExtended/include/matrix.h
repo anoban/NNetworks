0a3d6b4042548961dc610ee951bbc9004ae11b1d
anoban <2016sb117@univ.jfn.ac.lk>
2024-04-03 11:35:01 +0530

    modified:   Fashion-MNIST/NNExtended/.clang-tidy 	modified:   Fashion-MNIST/NNExtended/include/matrix.h 	modified:   Fashion-MNIST/NNExtended/src/matrix.c

--- Fashion-MNIST/NNExtended/include/matrix.h
+++ Fashion-MNIST/NNExtended/include/matrix.h
@@ -1,11 +1,12 @@
 #pragma once
+#if defined __clang__ || defined __llvm__
+    #error "matrix.c uses MSVC AVX512 intrinsics, incompatible with LLVM intrinsics!"
+#endif // !__clang__ || defined __llvm__
+
 #ifndef __MATRIX_H__
     #define __MATRIX_H__
     #include <stdbool.h>
     #include <stdint.h>
-
-// enum to specify the type of binary operation between the matrix and a scalar (float)
-typedef enum MATRIX_SCALAR_OPERATION { ADDITION = 0x0A0B0C, SUBTRACTION, DIVISION, MULTIPLICATION } MATRIX_SCALAR_OPERATION_KIND;
 
 typedef struct shape {
         size_t nrows;    // number of rows in the matrix
@@ -18,12 +19,17 @@ typedef struct matrix {
 } matrix_t;
 
 // perform binary operations on all elements of the matrix.
-matrix_t MatrixOpScalar(
-    _Inout_ matrix_t                        matrix,
-    _In_ const float                        scalar,
-    _In_ const MATRIX_SCALAR_OPERATION_KIND operation,
-    _In_ const bool inplace /* whether to store the results in the original buffer or to return a new matrix object */
+matrix_t MatrixAddScalar(
+    _Inout_ matrix_t matrix,
+    _In_ const float scalar,
+    _In_ const bool  inplace /* whether to store the results in the original buffer or to return a new matrix object */
 );
+
+matrix_t MatrixSubScalar(_Inout_ matrix_t matrix, _In_ const float scalar, _In_ const bool inplace);
+
+matrix_t MatrixMulScalar(_Inout_ matrix_t matrix, _In_ const float scalar, _In_ const bool inplace);
+
+matrix_t MatrixDivScalar(_Inout_ matrix_t matrix, _In_ const float scalar, _In_ const bool inplace);
 
 // matrix - matrix multiplication.
 matrix_t MatrixMul(_In_ const matrix_t first, _In_ const matrix_t second);
--- Fashion-MNIST/NNExtended/src/matrix.c
+++ Fashion-MNIST/NNExtended/src/matrix.c
@@ -4,15 +4,15 @@
 
 #include <errhandlingapi.h>
 #include <handleapi.h>
-#include <stdio.h>
 #include <heapapi.h>
 #include <matrix.h>
+#include <stdio.h>
+#include <zmmintrin.h>
 
-matrix_t MatrixOpScalar(
-    _Inout_ matrix_t                        matrix,
-    _In_ const float                        scalar,
-    _In_ const MATRIX_SCALAR_OPERATION_KIND operation,
-    _In_ const bool inplace /* whether to store the results in the original buffer or to return a new matrix object */
+matrix_t MatrixAddScalar(
+    _Inout_ matrix_t matrix,
+    _In_ const float scalar,
+    _In_ const bool  inplace /* whether to store the results in the original buffer or to return a new matrix object */
 ) {
     matrix_t tmp    = { 0 };
     float*   buffer = NULL; // in case inplace = false
@@ -27,29 +27,29 @@ matrix_t MatrixOpScalar(
             ;
             return tmp;
         }
-    } else
-        buffer = matrix.buffer; // aliasing
-
-    switch (operation) {
-        case ADDITION :
-            for (size_t i = 0; i < matrix.dimension.nrows * matrix.dimension.ncolumns; ++i) buffer[i] = matrix.buffer[i] + scalar;
-            break;
-        case SUBTRACTION :
-            for (size_t i = 0; i < matrix.dimension.nrows * matrix.dimension.ncolumns; ++i) buffer[i] = matrix.buffer[i] - scalar;
-            break;
-        case MULTIPLICATION :
-            for (size_t i = 0; i < matrix.dimension.nrows * matrix.dimension.ncolumns; ++i) buffer[i] = matrix.buffer[i] * scalar;
-            break;
-        case DIVISION :
-            for (size_t i = 0; i < matrix.dimension.nrows * matrix.dimension.ncolumns; ++i) buffer[i] = matrix.buffer[i] / scalar;
-            break;
-        default : break;
     }
+    const __m512 scalar_broadcast = { .m512_f32 = { scalar } }; // broadcast the scalar to an array of 16 floats
+    __m512*      zmmword_0;
+
+    const size_t nfp32szmm = sizeof(__m512) / sizeof(float);
+    size_t       i         = 0;
+
+    for (; i < matrix.dimension.nrows * matrix.dimension.ncolumns; i += nfp32szmm) {
+        buffer[i] = matrix.buffer[i] / scalar;
+        zmmword_0 = (__m512*) (matrix.buffer + i);
+        ;
+    }
+
     return matrix;
 }
 
+matrix_t MatrixSubScalar(_Inout_ matrix_t matrix, _In_ const float scalar, _In_ const bool inplace) { }
+
+matrix_t MatrixMulScalar(_Inout_ matrix_t matrix, _In_ const float scalar, _In_ const bool inplace) { }
+
+matrix_t MatrixDivScalar(_Inout_ matrix_t matrix, _In_ const float scalar, _In_ const bool inplace) { }
+
 matrix_t MatrixMul(_In_ const matrix_t first, _In_ const matrix_t second) {
-    
     matrix_t result = { 0 };
 
     // matrix multiplication can only work if the number of columns of the first matrix is equal to the number of rows of the second matrix.
@@ -65,25 +65,23 @@ matrix_t MatrixMul(_In_ const matrix_t first, _In_ const matrix_t second) {
             second.dimension.ncolumns
         );
         return result;
-    }  
+    }
 
-    const HANDLE64 hProcHeap    = GetProcessHeap(); // sidestepping malloc, will probably hurt the performance
+    const HANDLE64 hProcHeap = GetProcessHeap(); // sidestepping malloc, will probably hurt the performance
     if (hProcHeap == INVALID_HANDLE_VALUE) {
         fputws(L"Error in MatrixMul, call to GetProcessHeap returned INVALID_HANDLE_VALUE", stderr);
         return result;
     }
 
     // number of elements in the product of matrix multiplication
-    const size_t   nreselems    = first.dimension.nrows * second.dimension.ncolumns;
+    const size_t nreselems = first.dimension.nrows * second.dimension.ncolumns;
 
-    float* const   buffer       = HeapAlloc(hProcHeap, 0, nreselems * sizeof(float));
+    float* const buffer    = HeapAlloc(hProcHeap, 0, nreselems * sizeof(float));
     if (!buffer) {
         fwprintf_s(stderr, L"Error %4lu in MatrixMul, call to HeapAlloc failed\n", GetLastError());
         return result;
     }
-    
+
     // the resulting matrix will have the number of rows of the first matrix and the number of columns of the second matrix.
-    const shape_t  result_shape = { .nrows = first.dimension.nrows, .ncolumns = second.dimension.ncolumns };
-    
-    
+    const shape_t result_shape = { .nrows = first.dimension.nrows, .ncolumns = second.dimension.ncolumns };
 }
