a1f77b16f2996f055a09252e2f81e5b86682d5d9
anoban <2016sb117@univ.jfn.ac.lk>
2024-07-18 21:21:12 +0530

    deleted:    NNMinimal/IdxUtils.py 	deleted:    NNMinimal/NNMinimal.py 	deleted:    NNMinimal/NNSUtils.py 	deleted:    NNMinimal/main.py 	modified:   NNMinimal/src/main.py

--- NNMinimal/src/main.py
+++ NNMinimal/src/main.py
@@ -16,6 +16,25 @@ def main() -> None:
 
     print(f"Train-set accuracy: {model.accuracy_score(train_images.data, train_labels.data):.6f}")
     print(f"Test-set accuracy: {model.accuracy_score(test_images.data, test_labels.data):.6f}")
+    
+    # Fashion-MNIST training data
+    train_images = Idx3(r"../idx/train-images-idx3-ubyte")
+    train_labels = Idx1(r"../idx/train-labels-idx1-ubyte")
+
+    # Fashion-MNIST test data
+    test_images = Idx3(r"../idx/t10k-images-idx3-ubyte")
+    test_labels = Idx1(r"../idx/t10k-labels-idx1-ubyte")
+
+    model = NNetworkMinimal(nodes_in=784, nodes_hid=10, nodes_out=10, maxiterations=5000, alpha=0.1)
+    model.gradient_descent(train_images.data, train_labels.data)
+
+    train_ascore: float = model.accuracy_score(train_images.data, train_labels.data)
+    test_ascore: float = model.accuracy_score(test_images.data, test_labels.data)
+
+    print(f"Accuracy score for training dataset: {train_ascore:5f}")
+    print(f"Accuracy score for test dataset:     {test_ascore:5f}")
+
+    model.save("./fmnist")
 
 
 def evaluate(filepath: str) -> None:
