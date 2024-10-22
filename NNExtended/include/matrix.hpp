374a34e87a921a708f572c52d8d5d2817197bb0a
anoban <2016sb117@univ.jfn.ac.lk>
2024-10-22 18:19:17 +0530

    intrinsics

--- NNExtended/include/matrix.hpp
+++ NNExtended/include/matrix.hpp
@@ -2,6 +2,8 @@
 #include <concepts>
 #include <utility>
 
+#include <immintrin.h>
+
 #include <iterator.hpp>
 #include <utilities.hpp>
 
@@ -10,26 +12,26 @@
 // Eigen is too messy for my taste, cba with BLAS and it's fortran dependencies and oneMKL requires about 6 GiBs of space on disk
 // so yes we will be rolling our own!
 
-template<typename _Ty> requires std::floating_point<_Ty> class matrix final {
-    public:
-        using value_type            = _Ty; // can be float, double or long double
-        using pointer               = _Ty*;
-        using const_pointer         = const _Ty*;
-        using refernce              = _Ty&;
-        using const_refernce        = const _Ty&;
+class matrix final {
+    private:
+        using value_type            = float;
+        using pointer               = value_type*;
+        using const_pointer         = const value_type*;
+        using refernce              = value_type&;
+        using const_refernce        = const value_type&;
         using size_type             = unsigned long long;
         using difference_type       = long long;
-        using iterator              = random_access_iterator<_Ty>;
-        using const_iterator        = random_access_iterator<const _Ty>;
-        using column_iterator       = strided_random_access_iterator<_Ty>;
-        using const_column_iterator = strided_random_access_iterator<const _Ty>;
+        using iterator              = random_access_iterator<value_type>;
+        using const_iterator        = random_access_iterator<const value_type>;
+        using column_iterator       = strided_random_access_iterator<value_type>;
+        using const_column_iterator = strided_random_access_iterator<const value_type>;
 
         // clang-format off
-    #ifndef __TEST__ // if __TEST__ is defined, make these private member variables public, for testing
-    private:
+    #ifdef __TEST__ // if __TEST__ is defined, make these private member variables public for testing
+    public:
     #endif //
         // clang-format on
-        _Ty*      _buffer; // internal buffer
+        pointer   _buffer; // internal buffer
         size_type _nrows;
         size_type _ncols;
 
@@ -103,5 +105,57 @@ template<typename _Ty> requires std::floating_point<_Ty> class matrix final {
 
         inline const_iterator cend() const noexcept {
             return { _buffer /* resource pointer */, _nrows * _ncols /* length */, _nrows * _ncols /* offset */ };
+        }
+
+        inline pointer data() noexcept { return _buffer; }
+
+        inline const_pointer data() const noexcept { return _buffer; }
+
+        // choosing to implement matrix multiplication as a non-member function instead of overloading operator*() or
+        // operator*=() because these approaches will compromise on performance
+        // in a sitiation where an expression like this matrix = matrix_0 * matrix_1 gets iteratively executed,
+        // we'll pay a lot for the gratuitous memory allocations for the matrix multiplication product
+        // providing a custom move assignment operator cannot help us prevent the allocation needed to store the product before it is returned by operator whatnot()
+        // this is acceptable if these operators get called once or twice, but during gradient descent, these kind of expressions will be evaluated
+        // tens of thousands of times iteratively, where the dimensions of the matrices do not change!
+        // by using a friend function, we can use a third argument as an in-out parameter where the multiplication product can be
+        // materialized, so once all the matrices have been allocated with memory, we could reuse the same buffers throughout all the
+        // iterations
+        friend inline bool dotprod(_In_ const matrix& left, _In_ const matrix& right, _Inout_ matrix& prod) noexcept {
+            // VALIDATE THE PRECONDITIONS
+
+            // number of columns in the left matrix must be equal to the number of rows in the right matrix
+            if (left._ncols != right._nrows) { // cannot perform a matrix multiplication without this
+                ::fwprintf_s(stderr, L"");
+                return false;
+            }
+
+            [[likely]] // because we ain't that stupid
+            // if the result does not have enough memory to store the dot prduct, do a reallocation
+            // hopefully this newly allocated buffer can be reused throughout all the iterations
+            // the dot product will have the same number of rows as the left matrix and same number of columns as the right matrix
+            if (left._nrows * right._ncols > prod._nrows * prod._ncols) { // perform a reallocation
+                delete[] prod._buffer;                                    // lose the old buffer, if one existed
+                prod._buffer = new (std::nothrow) matrix::value_type[left._nrows * right._ncols];
+
+                if (!prod._buffer) {
+                    ::fwprintf_s(stderr, L"");
+                    prod._buffer = nullptr;
+                    prod._nrows = prod._ncols = 0;
+                    return false;
+                }
+
+                [[likely]] prod._nrows = left._nrows;
+                prod._ncols            = right._ncols;
+            }
+
+            // NOW TO THE ACTUAL MATRIX MULTIPLICATION
+
+            // number of value_types that can be packed in a 512 bits wide zmm register
+            const size_t zmm_packable_valtype_count { sizeof(__m512) / sizeof(matrix::value_type) };
+
+            for (size_t i = 0; i < left._nrows /* number of rows in the dotproduct */; ++i) {
+                //
+            }
         }
 };
