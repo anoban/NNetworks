ed9a7e4f9720b2779624c55f7f10c10571aa50bd
anoban <2016sb117@univ.jfn.ac.lk>
2025-06-14 12:13:46 +1000

    modified:   NNTorch/data.py

--- NNTorch/data.py
+++ NNTorch/data.py
@@ -1,15 +1,22 @@
 from typing import Any, override
 
+import numpy as np
+from numpy.typing import NDArray
 from torch.utils.data import Dataset
 
 
-class Idx1(Dataset):
+class Idx1(Dataset[NDArray[np.uint8]]):
     """
     #
     """
 
-    def __init__(self) -> None:
+    def __init__(self, filepath: str) -> None:
         super().__init__()
+        try:
+            with open(file=filepath, mode="rb") as fp:
+                temp = np.fromfile(file=fp, dtype=np.uint8)
+        except FileNotFoundError as error:
+            raise RuntimeError("") from error
 
     def __len__(self) -> int:
         return 5
